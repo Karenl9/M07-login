90557d59a707634c4895b8192f4fd2c394605baa
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2023-10-22 23:14:18 +0200

    conf bbdd

---
+++ bdConf.php
@@ -0,0 +1,9 @@
+<?php
+//Conexión de BD
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "users");
+    //define("PORT", "3306");
+
+?>
